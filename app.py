41a4f03aafdc58449a2587da0854569830d66f27
anajikadam <anaji.kadam@paramount.healthcare>
2021-05-14 10:42:45 +0530

    forth commit

--- app.py
+++ app.py
@@ -1,58 +1,51 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 
+
+# app = dash.Dash(__name__, suppress_callback_exceptions=True)
 from server import app, server
 
-from datetime import datetime
-import time 
-
 app.layout = html.Div([
-    # represents the URL bar, doesn't render anything
     dcc.Location(id='url', refresh=False),
-    dcc.Link('Generat Report'
-                    , href='/generat_report'),
-    html.Br(),
-    dcc.Link('PDF Generate'
-                    , href='/generat_pdf'),
-    html.Br(),
-    dcc.Link('Report sent email'
-                    , href='/Latest_Report_sent_email'),
-    # content will be rendered in this element
-    html.Div(id='page-content')
+    html.Div(id='main-page-content')
 ])
 
-@app.callback(dash.dependencies.Output('page-content', 'children'),
+
+index_page = html.Div([
+    dcc.Link('Go to Page 1', href='/page-1'),
+    html.Br(),
+    dcc.Link('Go to Page 2', href='/page-2'),
+])
+from app1 import layout1
+page_1_layout = html.Div([
+    html.H1('Page 1'),
+    dcc.Link(html.I(className="fa fa-home fa-2x"), href='/'),
+    html.Hr(),
+    layout1,
+    ])
+
+from layout_1 import appMain_layout
+page_2_layout = html.Div([
+    html.H1('Page 2'),
+    dcc.Link(html.I(className="fa fa-home fa-2x"), href='/'),
+    html.Hr(),
+    appMain_layout,
+    html.Br(),
+])
+
+# Update the index
+@app.callback(dash.dependencies.Output('main-page-content', 'children'),
               [dash.dependencies.Input('url', 'pathname')])
 def display_page(pathname):
-    if ('sent_email' in pathname):
-        print("flag0.0")
-        from Email import sendEmail
-        sendEmail()
-        return html.Div([
-            html.H3('You are on page of sending report through Email'),])
+    if pathname == '/page-1':
+        return page_1_layout
+    elif pathname == '/page-2':
+        return page_2_layout
+    else:
+        return index_page
 
-    if ('at_report' in pathname):
-        print("flag0")
-        from layout_1 import appMain_layout
-        return appMain_layout
-
-    if ('pdf' in pathname):
-        print("flag1")
-        x = datetime.now()
-        file_name = str(x.date())+'_'+str(x.strftime("%X").replace(':','-'))
-        filename = "assets/pdf/CovidReport_"+file_name+".pdf"
-        from PDFgen import PDFgenerate
-        mObj = PDFgenerate()
-        mObj.create_pdf_report(filename)
-        filename1 = "pdf/CovidReport_"+file_name+".pdf"
-        dwn_filename = "CovidReport_"+file_name+".pdf"
-        return html.Div([
-            html.H3('You are on page PDF Generate {}'.format(filename)),
-            html.A("PDF File",download=dwn_filename, href=app.get_asset_url("{}".format(filename1))),])
-    if len(pathname)<9:
-        return html.Div([html.H3('You are on page {}'.format(pathname)),   ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
